43c38c2e301a9062f7b16d5046fa6b25a21f4532
crafter126 <chanakas.dev@gmail.com>
2026-06-26 06:34:49 +0530

    create a shell script to start the app and create TextNode class to handle inline text nodes

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    print(TextNode("This is some anchor text", TextType.LINK, "https://www.google.com"))
+
+
+if __name__ == "__main__":
+    main()
