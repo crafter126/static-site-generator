55a5b9983405422c7f92b476ec3bab08ed60258a
crafter126 <chanakas.dev@gmail.com>
2026-07-01 21:21:02 +0530

    create extract markdown images and links functionality and write tests for it

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+)
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -72,6 +76,45 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_two(self):
+        matches = extract_markdown_images(
+            "This is text with an ![first image](https://i.imgur.com/zjjcJKZ.png), and this is also ![second image](https://i.imgur.com/zjjcIOD.png)"
+        )
+        self.assertEqual(
+            [
+                ("first image", "https://i.imgur.com/zjjcJKZ.png"),
+                ("second image", "https://i.imgur.com/zjjcIOD.png"),
+            ],
+            matches,
+        )
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)."
+        )
+        self.assertEqual(
+            [("to boot dev", "https://www.boot.dev")],
+            matches,
+        )
+
+    def test_extract_markdown_links_two(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
         )
 
 
