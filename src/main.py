ebc414a5930af952447f3430bc04bbe16bcd2cec
crafter126 <chanakas.dev@gmail.com>
2026-09-11 16:30:11 +0530

    create static files copy functionality from source to destination path

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 
 def main():
-    print(TextNode("This is some anchor text", TextType.LINK, "https://www.google.com"))
+    print("deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("copying static files to public directory..")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 if __name__ == "__main__":
